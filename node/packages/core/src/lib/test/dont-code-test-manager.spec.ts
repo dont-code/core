35f25dee91e8ee48b68b8fc2d03325a8a953c368
Gérard Collin <gerard.collin@appliweb.net>
2022-08-15 00:29:28 +0200

    Report plugin initial run with dummy data

--- node/packages/core/src/lib/test/dont-code-test-manager.spec.ts
+++ node/packages/core/src/lib/test/dont-code-test-manager.spec.ts
@@ -1,4 +1,5 @@
-import { DontCodeTestManager } from '@dontcode/core';
+import {DontCodeTestManager, dtcde} from '@dontcode/core';
+import {firstValueFrom} from "rxjs";
 
 describe('Test Manager', () => {
   it('should generate correct changes', () => {
@@ -16,5 +17,31 @@ describe('Test Manager', () => {
     const resultPointer = change.pointer;
     expect(resultPointer?.positionInSchema).toBe('creation/entities/fields');
     expect(resultPointer?.containerPositionInSchema).toBe('creation/entities');
+  });
+
+  it ('should set dummy provider', (done) => {
+    DontCodeTestManager.addDummyProviderFromContent("creation/entities/a", [
+      {
+        "name": "Dummy1",
+        "value": 123
+      },
+      {
+        "name": "Dummy2",
+        "value": 1656
+
+      }
+    ]);
+    const ret = dtcde.getStoreManager().getProvider("creation/entities/a")?.searchEntities("creation/entities/a");
+    expect(ret).toBeTruthy();
+    if (ret != null) {
+      firstValueFrom(ret).then(result => {
+        expect(result).toHaveLength(2);
+        if (result != null) {
+          expect(result[1]?.name).toEqual("Dummy2");
+          expect(result[1]?.value).toStrictEqual(1656);
+        }
+        done();
+      })
+    }
   });
 });
