75eee48d1e39e4659480bad4a2d0e4a6e4bbb315
Gérard Collin <gerard.collin@appliweb.net>
2023-01-26 09:57:44 +0100

    Core is now package as umd AND ems

--- node/tools/getRollupOptions.js
+++ node/tools/getRollupOptions.js
@@ -1,17 +1,29 @@
 function getRollupOptions(/** @type {import('rollup').RollupOptions} */ options) {
   // Following options are overridden:
   // - `sourcemap` to enable sourcemaps
-
   if (Array.isArray(options.output)) {
     options.output.forEach(o => {
       o.sourcemap = true;
     });
   } else {
-    options.output = {
+    //console.log("Config before:", options.output);
+    options.output = [{
       ...options.output,
+      format:"umd",
+      // file: options.output.dir+'/index.js',
       sourcemap: true,
-    };
+    },
+      {
+        ...options.output,
+        format:"es",
+        //file: options.output.dir+'/index.mjs',
+        sourcemap: true,
+        entryFileNames: '[name].mjs',
+        chunkFileNames: '[name].mjs',
+      }];
+
   }
+//  console.log("Config after:", options.output);
 
   return options;
 }
