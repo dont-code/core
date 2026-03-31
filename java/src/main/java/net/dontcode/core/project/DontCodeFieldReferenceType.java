e832a08e77c62516d99400f114a66ce4255b0019
Gérard Collin <gerard.collin@appliweb.net>
2026-03-31 18:31:52 +0200

    fix: Error reading generated model

--- java/src/main/java/net/dontcode/core/project/DontCodeFieldReferenceType.java
+++ java/src/main/java/net/dontcode/core/project/DontCodeFieldReferenceType.java
@@ -3,6 +3,7 @@ package net.dontcode.core.project;
 import com.fasterxml.jackson.annotation.JsonValue;
 
 public enum DontCodeFieldReferenceType {
+    ManyToOne ("ManyToOne"),
     OneToMany ("OneToMany");
 
     private String value;
